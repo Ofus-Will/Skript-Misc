49eb537a662d9f5240538f4a2947e365ee72c88c
Will-Ofus <billy.bowes@yo61.com>
2025-03-03 21:35:43 +0000

    added credit at start of compiled file

--- export.py
+++ export.py
@@ -21,12 +21,11 @@ def compile_scripts(folder_path, output_filename):
             file_path = os.path.join(folder_path, filename)
             
             with open(file_path, 'r', encoding='utf-8') as infile:
-                if not first:
-                    outfile.write("\n")
-                else:
+                if first:
+                    outfile.write("# written by: Ofus (Discord: OfusWill)\n")
                     first = False
 
-                outfile.write(f"## [SECTION: {filename}]\n")
+                outfile.write(f"\n## [SECTION: {filename}]\n")
                 outfile.write(infile.read().strip() + "\n\n")
 
     print(f"Compiled scripts into {output_path}")
